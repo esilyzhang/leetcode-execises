fff95327752a801a44f3664e5341e3abb1bbc20a
SmallPotatoCZ <sp.zhangchao@gmail.com>
2020-11-05 10:53:45 +0800

    完成 Shell 部分

--- Shell/word-frequency.sh
+++ Shell/word-frequency.sh
@@ -2,12 +2,13 @@
 # 读取数据
 declare -A word_=()
 while IFS= read -ra LINE || [ -n "${LINE}" ]; do
+  LINE=$(echo $LINE | sed "s/[\W]/ /g")
   for cell in $LINE; do
     if [[ ! -z ${word_[$cell]} ]]; then
       word_[$cell]=$((word_[$cell] + 1))
     else
       word_[$cell]=1
-      words+="${cell/\\\\n/ /} "
+      words+="%${cell}% "
     fi
   done
 done <words.txt
@@ -17,22 +18,31 @@ function max() {
   if [[ ! -z $@ ]]; then
     next_list=''
     max_name=''
+    item_name=''
     max=0
     for item in $@; do
-      if [[ max -lt ${word_[$item]} ]]; then
-        max=${word_[$item]}
-        max_name=$item
+      item_name=$(echo $item | sed "s/%//g")
+      if [[ max -lt ${word_[$item_name]} ]]; then
+        max=${word_[$item_name]}
+        max_name=$item_name
       fi
       next_list+="${item} "
     done
     echo "${max_name} ${max}"
-    max ${next_list/$max_name/}
+    next_list=$(echo $next_list | sed "s/%$max_name%/ /g")
+    max $next_list
   fi
 }
 
 # 遍历降序输出
-# max $words
-echo $words
+max $words
 # words.txt
 # the day is sunny the the
 # the sunny is is
+
+# words.txt #2
+# how many cans can a canner can if a canner can can cans\na canner can can as many cans as a canner can if a canner can can a can\na canner can can as many cans as a canner can cans
+
+# result
+# Runtime: 72 ms, faster than 6.96% of Bash online submissions for Word Frequency.
+# Memory Usage: 3.7 MB, less than 57.25% of Bash online submissions for Word Frequency.
