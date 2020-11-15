0c8902d87ea2a92689a598402c73a544be8877e0
SmallPotatoCZ <sp.zhangchao@gmail.com>
2020-11-15 20:38:27 +0800

    #76

--- JavaScript/src/dictionary/hard/76-minimum-window-substring.js
+++ JavaScript/src/dictionary/hard/76-minimum-window-substring.js
@@ -2,7 +2,44 @@
  * @param {string} s
  * @param {string} t
  * @return {string}
+ * @result Runtime: 104 ms, faster than 61.58% of JavaScript online submissions for Minimum Window Substring.
+ * Memory Usage: 43.6 MB, less than 18.23% of JavaScript online submissions for Minimum Window Substring.
  */
-var minWindow = function (s, t) {};
+var minWindow = function (s, t) {
+  let result = '',
+    lindex = 0,
+    rindex = 0,
+    charTypeNum;
+  const charMap = new Map();
+  for (let i = 0; i < t.length; i++) {
+    charMap.set(
+      t.charAt(i),
+      charMap.has(t.charAt(i)) ? charMap.get(t.charAt(i)) + 1 : 1
+    );
+  }
+  charTypeNum = charMap.size;
+  while (rindex < s.length) {
+    const rChar = s.charAt(rindex);
+    if (charMap.has(rChar)) {
+      charMap.set(rChar, charMap.get(rChar) - 1);
+      if (charMap.get(rChar) === 0) charTypeNum--;
+    }
+    while (charTypeNum === 0) {
+      curSubstr = s.substring(lindex, rindex + 1);
+      if (!result || result.length > curSubstr.length) {
+        result = curSubstr;
+      }
+      const lChar = s.charAt(lindex);
+      if (charMap.has(lChar)) {
+        charMap.set(lChar, charMap.get(lChar) + 1);
+        if (charMap.get(lChar) === 1) charTypeNum++;
+      }
+      lindex++;
+    }
+
+    rindex++;
+  }
+  return result;
+};
 
 module.exports = minWindow;
--- JavaScript/test/dictionary/hard/76-minimum-window-substring-test.js
+++ JavaScript/test/dictionary/hard/76-minimum-window-substring-test.js
@@ -1,6 +1,60 @@
 const expect = require('chai').expect;
 const minWindow = require('../../../src/dictionary/hard/76-minimum-window-substring');
 
-describe('', function () {
-  it('', function () {});
+describe('寻找包含字符串中所有字符的最短子字符串', function () {
+  it('ADOBECODEBANC 中包括 ABC 所有字符的字符串是 BANC', function () {
+    const s = 'ADOBECODEBANC',
+      t = 'ABC';
+    const result = 'BANC';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
+
+  it('a 中包括 a 所有字符的字符串是 a', function () {
+    const s = 'a',
+      t = 'a';
+    const result = 'a';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
+
+  it('ADOBECDYEBANC 中包括 ECD 所有字符的字符串是 ECD', function () {
+    const s = 'ADOBECDYEBANC',
+      t = 'ECD';
+    const result = 'ECD';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
+
+  it('ADOBECODEBANC 中包括 EFG 所有字符的字符串是空', function () {
+    const s = 'ADOBECODEBANC',
+      t = 'EFG';
+    const result = '';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
+
+  it('ab 中包括 b 所有字符的字符串是 b', function () {
+    const s = 'ab',
+      t = 'b';
+    const result = 'b';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
+
+  it('abcd 中包括 bd 所有字符的字符串是 bcd', function () {
+    const s = 'abcd',
+      t = 'bd';
+    const result = 'bcd';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
+
+  it('aa 中包括 aa 所有字符的字符串是 aa', function () {
+    const s = 'aa',
+      t = 'aa';
+    const result = 'aa';
+
+    expect(minWindow(s, t)).to.be.equal(result);
+  });
 });
